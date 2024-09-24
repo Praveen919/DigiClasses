70ca1628a07934c4f1b435f773e209a898805dda
Praveen <69238094+Praveen919@users.noreply.github.com>
2024-09-25 02:04:25 +0530

    Update feedbacksModel.js

--- backend/models/feedbacksModel.js
+++ backend/models/feedbacksModel.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
-    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },  //removed required: true
+    teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // added for tchr side feedback to post to admin
     subject: { type: String, required: true },
     feedback: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
