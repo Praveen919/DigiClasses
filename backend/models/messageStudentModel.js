4786c1cf9c45371db3b7a8dc569d41a87c85f889
Praveen <69238094+Praveen919@users.noreply.github.com>
2024-10-25 01:27:48 +0530

    Update messageStudentModel.js

--- backend/models/messageStudentModel.js
+++ backend/models/messageStudentModel.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Schema for messages sent from admin to student
 const MessageStudentSchema = new mongoose.Schema({
+  adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
   studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   subject: { type: String, required: true },
   message: { type: String, required: true },
@@ -13,6 +14,16 @@ const StudentToAdminTeacherMessageSchema = new mongoose.Schema({
   senderStudentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   recipientId: { type: mongoose.Schema.Types.ObjectId, ref: 'AdminTeacher', required: true },
   subject: { type: String, required: true },
+  message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Schema for messages sent from teacher to admin
+const TeacherToAdminMessageSchema = new mongoose.Schema({
+  teacherId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
+  //senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Teacher', required: true },
+  adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'Admin', required: true },
+  title: { type: String, required: true },
   message: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
@@ -45,6 +56,7 @@ const ExamNotificationSchema = new mongoose.Schema({
 
 // Schema for messages sent from admin to teacher
 const adminToTeacherMessageSchema = new mongoose.Schema({
+  adminId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
   teacherId: {type: mongoose.Schema.Types.ObjectId,ref: 'Teacher', required: true,},
   subject: {type: String,required: false,},
   message: {type: String,required: true,},
@@ -56,6 +68,8 @@ const adminToTeacherMessageSchema = new mongoose.Schema({
 const MessageStudent = mongoose.model('MessageStudent', MessageStudentSchema);
 const StudentToAdminTeacherMessage = mongoose.model('StudentToAdminTeacherMessage', StudentToAdminTeacherMessageSchema);
 const TeacherToStudentMessage = mongoose.model('TeacherToStudentMessage', TeacherToStudentMessageSchema);
+const TeacherToAdminMessage = mongoose.model('TeacherToAdminMessage', TeacherToAdminMessageSchema);
+
 const TeacherToStaffMessage = mongoose.model('TeacherToStaffMessage', TeacherToStaffMessageSchema);
 const ExamNotification = mongoose.model('ExamNotification', ExamNotificationSchema);
 const AdminToTeacherMessage = mongoose.model('AdminToTeacherMessage', adminToTeacherMessageSchema);
@@ -64,6 +78,8 @@ module.exports = {
   MessageStudent,
   StudentToAdminTeacherMessage,
   TeacherToStudentMessage,
+  TeacherToAdminMessage,
+
   TeacherToStaffMessage,
   ExamNotification,
   AdminToTeacherMessage
